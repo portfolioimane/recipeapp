255cf3f353c56461c657961d0003e333a883b03f
blogimane <blogimane.g@gmail.com>
2020-03-09 21:33:47 +0000

    Updates

---
+++ precache-manifest.fdc4588709a0cb857e8f18ef9510e7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624e9a815292a34d2d33598bb0508225",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "2eb789cce274d79af0ea",
+    "url": "/recipeapp/static/css/main.3fb94dec.chunk.css"
+  },
+  {
+    "revision": "3f232e8f201a4a2af267",
+    "url": "/recipeapp/static/js/2.5d4feff6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recipeapp/static/js/2.5d4feff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb789cce274d79af0ea",
+    "url": "/recipeapp/static/js/main.fbbeb5d0.chunk.js"
+  },
+  {
+    "revision": "14ed1e689e3253d63d40",
+    "url": "/recipeapp/static/js/runtime-main.428aa187.js"
+  }
+]);
